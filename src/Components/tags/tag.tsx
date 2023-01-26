62d2a6790b9c78d910cf4fd76f0b84b6e263234c
sera <serdafim24@gmail.com>
2023-01-26 03:38:23 -0800

    ad

--- src/Components/tags/tag.tsx
+++ src/Components/tags/tag.tsx
@@ -6,31 +6,31 @@ const Tag = (tags: props) => {
   return (
     <>
       <div className="mt-[1vh] flex">
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold  text-gray-800 shadow hover:bg-gray-300">
           #Business
         </button>
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold text-gray-800  shadow hover:bg-gray-300">
           #Marketing
         </button>
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold text-gray-800  shadow hover:bg-gray-300">
           #Copywriting
         </button>
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold text-gray-800  shadow hover:bg-gray-300">
           #Text
         </button>
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold text-gray-800  shadow hover:bg-gray-300">
           #Video
         </button>
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold text-gray-800  shadow hover:bg-gray-300">
           #Image
         </button>
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold text-gray-800  shadow hover:bg-gray-300">
           #Game
         </button>
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold text-gray-800  shadow hover:bg-gray-300">
           #Research
         </button>
-        <button className="h-[3vh] rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-300">
+        <button className="h-[3vh] rounded-full bg-white py-2 px-4 font-semibold text-gray-800  shadow hover:bg-gray-300">
           #Voice
         </button>
       </div>
