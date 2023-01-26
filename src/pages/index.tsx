723cc5314adbcdd347fe3d83e3acc91fb22e6a26
Alex <albu.alexandru.h@gmail.com>
2023-01-26 09:04:16 +0200

    commit2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,10 @@ const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <>
-      
-    </>
+    <div className="bg-blue-100">
+      <h1 className="bg-blue-400 text-white rounded-bl-lg py-5 px-10 rounded-br-lg text-5xl">Blog A.I.</h1>
+      <center><h2>Filler</h2></center>
+    </div>
   );
 };
 
